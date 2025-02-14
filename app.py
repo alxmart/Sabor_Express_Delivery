89c020f91214f041daebbd9b7b2fe8561f668c99
alxmart <alxmartinez@gmail.com>
2025-02-14 19:05:07 -0300

    Using if / elif / else st

--- app.py
+++ app.py
@@ -13,9 +13,41 @@ print("2. Listar restaurante")
 print("3. Ativar restaurante")
 print("4. Sair")
 
-opcao_escolhida = input("Escolha uma opção: ")
-#print("Você escolheu a opção: ", opcao_escolhida)
-print(f"Você escolheu a opção: {opcao_escolhida}")
+# print("Você escolheu a opção: ", opcao_escolhida)
+
+# print(f"Você escolheu a opção: {opcao_escolhida}")
+
+# print(type(opcao_escolhida))
+
+# opcao_escolhida = int(opcao_escolhida)
+
+opcao_escolhida = int(input("Escolha uma opção: "))
+
+"""
+if opcao_escolhida == 1 :
+    print("Cadastrar restaurante")
+elif opcao_escolhida == 2 :
+    print("Listar restaurante")
+elif opcao_escolhida == 3 :
+    print("Ativar restaurante")
+else:
+    print("Encerrando o programa")
+
+    """
+
+if opcao_escolhida == 1 :
+    print("Cadastrar restaurante")
+elif opcao_escolhida == 2 :
+    print("Listar restaurante")
+elif opcao_escolhida == 3 :
+    print("Ativar restaurante")
+else:
+    print("Encerrando o programa")
+
+def finalizar_app():
+    print("Encerrando o programa")
+
+   
 
 
 
