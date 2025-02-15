8a8e5a2897ddea9959150cb95e3ace9c6d36ae60
alxmart <alxmartinez@gmail.com>
2025-02-15 11:38:44 -0300

    Implementing functions

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
+import os
 
-print("""
+def exibir_nome_do_programa():   
+    print("""
 ░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
 ██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
 ╚█████╗░███████║██████╦╝██║░░██║██████╔╝  █████╗░░░╚███╔╝░██████╔╝██████╔╝█████╗░░╚█████╗░╚█████╗░
@@ -8,46 +10,37 @@ print("""
 ╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░
 """)
 
-print("1. Cadastrar restaurante")
-print("2. Listar restaurante")
-print("3. Ativar restaurante")
-print("4. Sair")
-
-# print("Você escolheu a opção: ", opcao_escolhida)
-
-# print(f"Você escolheu a opção: {opcao_escolhida}")
-
-# print(type(opcao_escolhida))
-
-# opcao_escolhida = int(opcao_escolhida)
-
-opcao_escolhida = int(input("Escolha uma opção: "))
-
-"""
-if opcao_escolhida == 1 :
-    print("Cadastrar restaurante")
-elif opcao_escolhida == 2 :
-    print("Listar restaurante")
-elif opcao_escolhida == 3 :
-    print("Ativar restaurante")
-else:
-    print("Encerrando o programa")
-
-    """
-
-if opcao_escolhida == 1 :
-    print("Cadastrar restaurante")
-elif opcao_escolhida == 2 :
-    print("Listar restaurante")
-elif opcao_escolhida == 3 :
-    print("Ativar restaurante")
-else:
-    print("Encerrando o programa")
+def exibir_opcoes():
+    print("1. Cadastrar restaurante")
+    print("2. Listar restaurante")
+    print("3. Ativar restaurante")
+    print("4. Sair")
 
 def finalizar_app():
-    print("Encerrando o programa")
+    os.system('clear') # => Mac e Linux
+    # os.clear('cls') => WINDOWS
+    print("Finalizando o app") 
+    
+def escolher_opcao():
 
-   
+    opcao_escolhida = int(input("Escolha uma opção: "))
+    #opcao_escolhida = int(opcao_escolhida)   
+    
+    if opcao_escolhida == 1 :
+        print("Cadastrar restaurante")
+    elif opcao_escolhida == 2 :
+        print("Listar restaurante")
+    elif opcao_escolhida == 3 :
+        print("Ativar restaurante")
+    else:
+        finalizar_app()
+
+def main():
+    exibir_nome_do_programa()
+    exibir_opcoes()
+
+if __name__ == '__main__':
+    main()
 
 
 
--- tests.py
+++ tests.py
@@ -4,3 +4,14 @@ print(""" Sabor Express
       """)
 print("================")
 
+
+# print("Você escolheu a opção: ", opcao_escolhida)
+
+# print(f"Você escolheu a opção: {opcao_escolhida}")
+
+# print(type(opcao_escolhida))
+
+# opcao_escolhida = int(opcao_escolhida)
+
+
+
