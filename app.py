fb4af0f5b395b1d416556a158b93b277b468e1b1
alxmart <alxmartinez@gmail.com>
2025-04-29 18:29:53 -0300

    Refactoring code.

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
-import os
+#print("Hello World !")
 
-def exibir_nome_do_programa():   
-    print("""
+print("""
 ░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
 ██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
 ╚█████╗░███████║██████╦╝██║░░██║██████╔╝  █████╗░░░╚███╔╝░██████╔╝██████╔╝█████╗░░╚█████╗░╚█████╗░
@@ -10,37 +9,17 @@ def exibir_nome_do_programa():
 ╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░
 """)
 
-def exibir_opcoes():
-    print("1. Cadastrar restaurante")
-    print("2. Listar restaurante")
-    print("3. Ativar restaurante")
-    print("4. Sair")
+print('1.Cadastrar restaurante')
+print('2.Listar restaurante')
+print('3.Ativar restaurante')
+print('4.Sair\n')
 
-def finalizar_app():
-    os.system('clear') # => Mac e Linux
-    # os.clear('cls') => WINDOWS
-    print("Finalizando o app") 
-    
-def escolher_opcao():
+opcao_escolhida = input('Escolha uma opção: ')
+print(f'Você escolheu a opção {opcao_escolhida}')
 
-    opcao_escolhida = int(input("Escolha uma opção: "))
-    #opcao_escolhida = int(opcao_escolhida)   
-    
-    if opcao_escolhida == 1 :
-        print("Cadastrar restaurante")
-    elif opcao_escolhida == 2 :
-        print("Listar restaurante")
-    elif opcao_escolhida == 3 :
-        print("Ativar restaurante")
-    else:
-        finalizar_app()
 
-def main():
-    exibir_nome_do_programa()
-    exibir_opcoes()
 
-if __name__ == '__main__':
-    main()
+
 
 
 
