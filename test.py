7d82d060709faca60de86b59a98282968a6c67e7
alxmart <alxmartinez@gmail.com>
2026-04-18 10:31:33 -0300

    Adicionando Exercicio3

--- test.py
+++ test.py
@@ -1,1 +1,10 @@
-print("Testing")
+def configurar_tempo_foco():
+    import os
+    #os.system('clear')
+    tempo = int(input("Digite o tempo de foco (25-45 min): "))
+    if 25 <= tempo <= 45:
+        print("Tempo configurado para", tempo, "minutos.")
+    else:
+        print("Valor inválido. Configure um tempo entre 25 e 45 minutos.")
+
+configurar_tempo_foco()
