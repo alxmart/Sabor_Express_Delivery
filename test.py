7d4bcda5dad901b7b9b90d1a180ef237fdae29ab
alxmart <alxmartinez@gmail.com>
2026-04-18 18:40:39 -0300

    Renaming files

--- test.py
+++ test.py
@@ -1,7 +1,1 @@
 
-encomendas = input("Digite os números das encomendas separados por vírgula: ").split(',')
-try:
-    for encomenda in encomendas:
-        print(int(encomenda))
-except ValueError:
-    print("Uma das entradas não é um número válido.")
