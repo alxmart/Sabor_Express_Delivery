e3dce5bce5f46f3bdb7025e32c9e86238730e854
alxmart <alxmartinez@gmail.com>
2026-04-18 08:15:00 -0300

    Adicionando Exercicio 1

---
+++ Exercicio_1.py
@@ -0,0 +1,29 @@
+"""
+Exercícios
+
+1 - Imprima a frase: Python na Escola de Programação da Alura.
+
+2 - Imprima a frase: Meu nome é {nome} e tenho {idade} anos em que nome e idade precisam ser valores
+ armazenados em variáveis.
+
+3 - Imprima a palavra: ‘ALURA’ de modo que cada letra fique em uma linha, como mostrado a seguir:
+
+4 - Imprima a frase: O valor arredondado de pi é: {pi_arredondado} em que o valor de pi precisa ser
+ armazenado em uma variável e arredondado para apenas duas casas decimais. 
+"""
+
+print('Python na Escola de Programação da Alura.')
+
+nome = input('Digite seu nome: ')
+
+idade = input('Digite sua idade')
+
+print(f'Meu nome é {nome} e tenho {idade} anos')
+
+print('A')
+print('L')
+print('U')
+print('R')
+print('A') 
+
+print(f'O valor arredondado de pi é: {round(3.141592653589793, 2)}')
