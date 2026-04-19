da00cb812064ade392cab4831c1ba169c562361d
alxmart <alxmartinez@gmail.com>
2026-04-19 12:53:12 -0300

    Adicionando lista de exercicios 6

---
+++ Lista_Exercicio_6.py
@@ -0,0 +1,88 @@
+"""
+       Como já vimos, programação é prática! Criamos mais uma lista de atividades (não obrigatórias) 
+    para você exercitar e reforçar ainda mais seu aprendizado e o conteúdo da vez são docstrings.
+       Bora praticar?
+       Exercícios
+"""
+
+# Crie uma docstring para a função exibir_nome_do_programa()
+def exibir_nome_do_programa():
+    '''
+    Esta função exibe o nome do programa
+    '''    
+
+# Crie uma docstring para a função exibir_opcoes()
+def exibir_opcoes():
+    '''
+     Exibe as opções disponíveis no menu principal 
+    ''' 
+
+# Crie uma docstring para a função finalizar_app()
+def finalizar_app():
+    ''' 
+    Exibe mensagem de finalização do aplicativo 
+    '''
+
+# Crie uma docstring para a função opcao_invalida()
+def opcao_invalida():
+    ''' 
+    Exibe mensagem de opção inválida e retorna ao menu principal 
+    
+    Outputs:
+    - Retorna ao menu principal
+    '''
+
+# Crie uma docstring para a função exibir_subtitulo(texto)
+def exibir_subtitulo(texto):
+    ''' 
+    Exibe um subtítulo estilizado na tela 
+    
+    Inputs:
+    - texto: str - O texto do subtítulo
+    '''
+
+# Crie uma docstring para a função cadastrar_novo_restaurante()
+def cadastrar_novo_restaurante(): 
+     ''' 
+     Essa função é responsável por cadastrar um novo restaurante 
+    
+    Inputs:
+    - Nome do restaurante
+    - Categoria
+
+    Outputs:
+    - Adiciona um novo restaurante a lista de restaurantes
+    '''
+
+# Crie uma docstring para a função listar_restaurantes()
+def listar_restaurantes():
+    ''' 
+    Lista os restaurantes presentes na lista 
+    
+    Outputs:
+    - Exibe a lista de restaurantes na tela
+    '''
+
+# Crie uma docstring para a função alternar_estado_restaurante()
+def alterar_estado_restaurante():
+   ''' 
+   Altera o estado ativo/desativado de um restaurante 
+    
+    Outputs:
+    - Exibe mensagem indicando o sucesso da operação
+    '''
+
+# Crie uma docstring para a função escolher_opcao()
+def alterar_estado_restaurante():
+     ''' 
+     Solicita e executa a opção escolhida pelo usuário 
+    
+    Outputs:
+    - Executa a opção escolhida pelo usuário
+    '''
+
+# Crie uma docstring para a função main()
+def main():
+     ''' 
+     Função principal que inicia o programa 
+     '''
---
+++ exemplo_docstring.py
@@ -0,0 +1,13 @@
+
+def buscar_receitas_por_categoria(categoria):
+    """
+    Busca receitas por categoria e retorna uma lista de receitas
+    
+    Argumentos:
+        categoria (str): A categoria da receita desejada
+    
+    Retorna:
+        list: Uma lista contendo as receitas da categoria especificada
+    """
+    # Código omitido para brevidade
+    pass
